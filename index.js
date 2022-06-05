1eedf55e2f4c20791f0ebdafae5f2846fa4966ab
pietro.dct2@gmail.com <pietro.dct2@gmail.com>
2022-06-05 18:05:22 -0500

    1

--- index.js
+++ index.js
@@ -1,36 +1,21 @@
 const express = require('express')
 const app = express()
-const myrouter=require('./src/route/router.js')
 const port = process.env.PORT || 3001
-const cors = require('cors')
 
-
-require('dotenv').config
-require('./connDb.js')
-
-
-
-//app.use(cors);
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
 
 
 
 app.listen(port, (error)=>{
 	
 		if (!error) {
-				console.log("App is running")
+				res.send("Working")
 			}
 	
 	
 		else {
-				console.log("App is not running")
+				res.send("Working")
 			}
 	
 	})
 
 
-app.use("/",myrouter)
